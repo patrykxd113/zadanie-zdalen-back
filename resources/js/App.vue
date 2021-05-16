1aa3546c3ef5eae6d328cb636a169083ac355b00
patrykxd113 <patrykxd2@gmail.com>
2021-05-17 00:09:04 +0200

    initial relase

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> 
+      <h1>Hello</h1>
+      <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './Components/HelloWorld'
+export default {
+    components:{
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+
+</style>
